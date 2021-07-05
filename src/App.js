e0bc06d15ec9f2a14b0d7b3b147dc40f4353dadf
hareesh-r <hareeshprogrammer@gmail.com>
2021-07-05 22:42:28 +0530

    left bar functional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [city, setCity] = useState("Chennai");
   const [JSON_RESPONSE, setJSON_RESPONSE] = useState([]);
+  const [country, setCountry] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [showData,setShowData] = useState(false);
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  useEffect(() => {
+    var timer = setInterval(() => setDate(new Date()), 1000)
+    return function cleanup() {
+      clearInterval(timer)
+    }
+  })
 
   const API_KEY = "590ef8542927784e438ff86038506500";
-  const BASE_URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-  const DAYS_URL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`;
+  const BASE_URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+  const DAYS_URL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`;
 
 
 
@@ -23,157 +34,66 @@ function App() {
 
     console.log(response);
 
+    setCountry(response.sys.country);
+
     setJSON_RESPONSE(response);
+
+    setShowData(true);
 
   }
 
   return (
-    <div id="app" className="App light">
+    <div id="app" className="App cloud">
       <div className="top">
         <div className="importantCity">
-        <div className="importantCities mumbai">
-          <img src="https://www.india-briefing.com/news/wp-content/uploads/2013/07/India-Briefing-Economy-of-Mumbai-Indias-Major-Commercial-Hub.jpg" alt="" />
-          <div className="sideGroup">
-            <h2>Mumbai</h2> <code>climate</code> <p>35°C</p>
+          <div className="importantCities mumbai">
+            <img src="https://www.india-briefing.com/news/wp-content/uploads/2013/07/India-Briefing-Economy-of-Mumbai-Indias-Major-Commercial-Hub.jpg" alt="" />
+            <div className="sideGroup">
+              <h2>Mumbai</h2> <code>climate</code> <p>35°C</p>
+            </div>
           </div>
-        </div>
-        <div className="importantCities delhi">
-          <img src="https://cdn.britannica.com/37/189837-050-F0AF383E/New-Delhi-India-War-Memorial-arch-Sir.jpg" alt="" />
-          <div className="sideGroup">
-            <h2>Delhi</h2> <code>climate</code> <p>35°C</p>
+          <div className="importantCities delhi">
+            <img src="https://cdn.britannica.com/37/189837-050-F0AF383E/New-Delhi-India-War-Memorial-arch-Sir.jpg" alt="" />
+            <div className="sideGroup">
+              <h2>Delhi</h2> <code>climate</code> <p>35°C</p>
+            </div>
           </div>
-        </div>
-        <div className="importantCities chennai">
-          <img src="https://im.rediff.com/news/2014/aug/25chennai1.jpg?w=670&h=900" alt="" />
-          <div className="sideGroup">
-            <h2>Chennai</h2> <code>climate</code> <p>35°C</p>
+          <div className="importantCities chennai">
+            <img src="https://im.rediff.com/news/2014/aug/25chennai1.jpg?w=670&h=900" alt="" />
+            <div className="sideGroup">
+              <h2>Chennai</h2> <code>climate</code> <p>35°C</p>
+            </div>
           </div>
-        </div>
-        <div className="importantCities kolkatta">
-          <img src="https://cdn.britannica.com/75/121075-050-CBF79FB6/Victoria-Statue-front-Memorial-Hall-Kolkata-West.jpg" alt="" />
-          <div className="sideGroup">
-            <h2>Kolatta</h2> <code>climate</code> <p>35°C</p>
+          <div className="importantCities kolkatta">
+            <img src="https://cdn.britannica.com/75/121075-050-CBF79FB6/Victoria-Statue-front-Memorial-Hall-Kolkata-West.jpg" alt="" />
+            <div className="sideGroup">
+              <h2>Kolatta</h2> <code>climate</code> <p>35°C</p>
+            </div>
           </div>
-        </div>
-        <div className="importantCities bangalore">
-          <img src="https://lp-cms-production.imgix.net/2019-06/9483508eeee2b78a7356a15ed9c337a1-bengaluru-bangalore.jpg" alt="" />
-          <div className="sideGroup">
-            <h2>Bangalore</h2> <code>climate</code> <p>35°C</p>
+          <div className="importantCities bangalore">
+            <img src="https://lp-cms-production.imgix.net/2019-06/9483508eeee2b78a7356a15ed9c337a1-bengaluru-bangalore.jpg" alt="" />
+            <div className="sideGroup">
+              <h2>Bangalore</h2> <code>climate</code> <p>35°C</p>
+            </div>
           </div>
-        </div>
         </div>
         <div className="search">
           <input type="text" onChange={(e) => { setCity(e.target.value) }} />
           <button onClick={() => { getResponse(BASE_URL, city) }}>Search</button>
         </div>
       </div>
-      <div className="left"> 
-          <h2>Mumbai , India</h2>
-          <h3>9:00 AM</h3>
-          <h4>30 May</h4>
-          <h5>Thursday</h5>
+      <div>
+        {showData ? (<div className="left"><h1>{city} , {country}</h1>
+          <h2>{date.toLocaleTimeString()}</h2>
+          <h2>{date.toLocaleDateString()}</h2>
+          <h4>{days[date.getDay()]}</h4>
+          <img src="" alt="" />
+          <h1>{JSON_RESPONSE.main.temp}°C</h1>
+          <h3>Min: {JSON_RESPONSE.main.temp_min}°C Max: {JSON_RESPONSE.main.temp_max}°C</h3></div>) 
+          : 
+          (<div>Sorry No data Found</div>)}
+
       </div>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
     </div>
   );
 }
