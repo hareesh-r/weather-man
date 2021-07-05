15f85c0114b2ff44420de62ca6254c075db52053
hareesh-r <hareeshprogrammer@gmail.com>
2021-07-05 23:47:18 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   const [JSON_RESPONSE, setJSON_RESPONSE] = useState([]);
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
-  const [showData,setShowData] = useState(false);
+  const [showData, setShowData] = useState(false);
   const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const [climateIcon,setClimateIcon] = useState("10d");
+  const [climateIcon, setClimateIcon] = useState("10d");
 
   useEffect(() => {
     var timer = setInterval(() => setDate(new Date()), 1000)
@@ -31,17 +31,19 @@ function App() {
 
     const response = await api_response.json();
 
-    console.log(city);
-
     console.log(response);
 
-    if(response.cod=="404")
-    {
+    if (response.cod == "404") {
       setShowData(false);
-    }else{
+
+    } else {
+
       setJSON_RESPONSE(response);
+
       setClimateIcon(response.weather[0].icon);
+
       setCountry(response.sys.country);
+
       setShowData(true);
     }
 
@@ -49,57 +51,121 @@ function App() {
 
   return (
     <div id="app" className="App cloud">
+
       <div className="top">
+
         <div className="importantCity">
-          <div onClick={()=>{setCity("mumbai");getResponse(BASE_URL, city)}} className="importantCities mumbai">
+
+          <div
+            onClick={() => { setCity("mumbai"); getResponse(BASE_URL, city) }}
+            className="importantCities mumbai">
+
             <img src="https://www.india-briefing.com/news/wp-content/uploads/2013/07/India-Briefing-Economy-of-Mumbai-Indias-Major-Commercial-Hub.jpg" alt="" />
+
             <div className="sideGroup">
+
               <h2>Mumbai</h2> <code>climate</code> <p>35°C</p>
+
             </div>
+
           </div>
-          <div onClick={()=>{setCity("delhi");getResponse(BASE_URL, city)}} className="importantCities delhi">
+
+          <div onClick={() => { setCity("delhi"); getResponse(BASE_URL, city) }} className="importantCities delhi">
             <img src="https://cdn.britannica.com/37/189837-050-F0AF383E/New-Delhi-India-War-Memorial-arch-Sir.jpg" alt="" />
+
             <div className="sideGroup">
+
               <h2>Delhi</h2> <code>climate</code> <p>35°C</p>
+
             </div>
+
           </div>
-          <div onClick={()=>{setCity("chennai");getResponse(BASE_URL, city)}} className="importantCities chennai">
+
+          <div onClick={() => { setCity("chennai"); getResponse(BASE_URL, city) }} className="importantCities chennai">
+
             <img src="https://im.rediff.com/news/2014/aug/25chennai1.jpg?w=670&h=900" alt="" />
             <div className="sideGroup">
+
               <h2>Chennai</h2> <code>climate</code> <p>35°C</p>
+
             </div>
+
           </div>
-          <div onClick={()=>{setCity("kolkata");getResponse(BASE_URL, city)}} className="importantCities kolkata">
+
+          <div onClick={() => { setCity("kolkata"); getResponse(BASE_URL, city) }} className="importantCities kolkata">
+
             <img src="https://cdn.britannica.com/75/121075-050-CBF79FB6/Victoria-Statue-front-Memorial-Hall-Kolkata-West.jpg" alt="" />
+
             <div className="sideGroup">
+
               <h2>Kolkata</h2> <code>climate</code> <p>35°C</p>
+
             </div>
+
           </div>
-          <div onClick={()=>{setCity("Bangalore");getResponse(BASE_URL, city)}} className="importantCities bangalore">
+
+          <div onClick={() => { setCity("Bangalore"); getResponse(BASE_URL, city) }} className="importantCities bangalore">
+
             <img src="https://lp-cms-production.imgix.net/2019-06/9483508eeee2b78a7356a15ed9c337a1-bengaluru-bangalore.jpg" alt="" />
+
             <div className="sideGroup">
+
               <h2>Bangalore</h2> <code>climate</code> <p>35°C</p>
+
             </div>
+
           </div>
+
         </div>
+
         <div className="search">
+
           <input type="text" onChange={(e) => { setCity(e.target.value) }} />
+
           <button onClick={() => { getResponse(BASE_URL, city) }}>Search</button>
+
         </div>
-      </div>
-      <div>
-        {showData ? (<div className="left"><h1>{city} , {country}</h1>
-          <h2>{date.toLocaleTimeString()}</h2>
-          <h2>{date.toLocaleDateString()}</h2>
-          <h4>{days[date.getDay()]}</h4>
-          <img src={`http://openweathermap.org/img/wn/${climateIcon}@2x.png`} alt="" />
-          <code>{JSON_RESPONSE.weather[0].description}</code>
-          <h1>{JSON_RESPONSE.main.temp}°C</h1>
-          <h3>Min: {JSON_RESPONSE.main.temp_min}°C Max: {JSON_RESPONSE.main.temp_max}°C</h3></div>) 
-          : 
-          (<div className="left"><h2>Sorry No data Found</h2></div>)}
 
       </div>
+
+      <div>
+
+        {showData
+          ?
+          (
+            <div className="left">
+
+              <h1>{city} , {country}</h1>
+
+              <h2>{date.toLocaleTimeString()}</h2>
+
+              <h2>{date.toLocaleDateString()}</h2>
+
+              <h4>{days[date.getDay()]}</h4>
+
+              <img src={`http://openweathermap.org/img/wn/${climateIcon}@2x.png`} alt="" />
+
+              <code>{JSON_RESPONSE.weather[0].description}</code>
+
+              <h1>{JSON_RESPONSE.main.temp}°C</h1>
+
+              <h3>Min: {JSON_RESPONSE.main.temp_min}°C Max: {JSON_RESPONSE.main.temp_max}°C</h3>
+
+            </div>
+
+          )
+          :
+          (
+            <div className="left">
+
+              <h2>Sorry No data Found</h2>
+
+            </div>
+          )
+        }
+
+      </div>
+
     </div>
   );
 }
